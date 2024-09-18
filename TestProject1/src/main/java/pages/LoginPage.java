5f47cc6aa0babc12ae1d7554f469a60fb5ff1206
venkig712 <138414395+venkig712@users.noreply.github.com>
2024-09-18 10:41:35 +1000

    Update LoginPage.java insterted loginpage 3 comment

--- TestProject1/src/main/java/pages/LoginPage.java
+++ TestProject1/src/main/java/pages/LoginPage.java
@@ -5,6 +5,7 @@ public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("loginpage");
 		System.out.println("loginpage1");
+		System.out.println("loginpage2");
 
 	}
 
